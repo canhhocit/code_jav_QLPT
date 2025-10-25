b9c377c4d8e07a19d887470d741c66bd260a568b
canhhocit <canhhuu26@gmail.com>
2025-10-25 17:36:13 +0700

    account

---
+++ app/src/main/java/com/example/logincustomer/data/DAO/account_DAO.java
@@ -0,0 +1,50 @@
+package com.example.logincustomer.data.DAO;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.logincustomer.data.DatabaseHelper.DatabaseHelper;
+import com.example.logincustomer.data.Model.Account;
+import com.example.logincustomer.data.Model.baocao_thuchi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class account_DAO {
+    private SQLiteDatabase db;
+    public account_DAO(Context context) {
+        DatabaseHelper databaseHelper = new DatabaseHelper(context);
+        db = databaseHelper.getWritableDatabase();
+    }
+
+    public void insertAccount(Account account){
+        ContentValues vlu = new ContentValues();
+        vlu.put("username", account.getUsername());
+        vlu.put("password",account.getPass());
+        db.insert("TaiKhoan",null,vlu);
+    }
+    public void updateAccount(Account account){
+        ContentValues vlu = new ContentValues();
+        vlu.put("username", account.getUsername());
+        vlu.put("password",account.getPass());
+        db.update("TaiKhoan",vlu,"idTK =?",new String[]{String.valueOf(account.getId())});
+    }
+    public void deleteAcoount(int id){
+        db.delete("TaiKhoan","idTK =?",new String[]{String.valueOf(id)});
+    }
+
+    public List<Account> getAllAccount(){
+        List<Account> list = new ArrayList<>();
+        Cursor cursor = db.rawQuery("Select * from TaiKhoan",null);
+        if (cursor.moveToFirst()) {
+            do {
+                list.add(new Account(cursor.getInt(0),
+                        cursor.getString(1),cursor.getString(2)));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return list;
+    }
+}
---
+++ app/src/main/java/com/example/logincustomer/data/Model/Account.java
@@ -0,0 +1,40 @@
+package com.example.logincustomer.data.Model;
+
+public class Account {
+    private int id;
+    private String username;
+    private String pass;
+
+    public Account(int id, String username, String pass) {
+        this.id = id;
+        this.username = username;
+        this.pass = pass;
+    }
+
+    public Account() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
